9a98cf472c40d20e08ad025262e1864c2bada7cc
Julius Lungys <juliuslungys@gmail.com>
2018-09-19 10:58:56 +0300

    add all missing enum unit tests

--- Battleships-gameTests/OceanTest.cpp
+++ Battleships-gameTests/OceanTest.cpp
@@ -9,3 +9,13 @@ TEST(Ocean, should_getIntactShipCell_when_usingGetCell) {
 	ocean.setCellAtCoordinates(1, 1, Cell::INTACT_SHIP);
 	EXPECT_EQ(ocean.getCellAtCoordinates(1, 1), Cell::INTACT_SHIP);
 }
+TEST(Ocean, should_getMissedShotCell_when_usingGetCell) {
+	Ocean ocean;
+	ocean.setCellAtCoordinates(1, 1, Cell::MISSED_SHOT);
+	EXPECT_EQ(ocean.getCellAtCoordinates(1, 1), Cell::MISSED_SHOT);
+}
+TEST(Ocean, should_getSunkenShipCell_when_usingGetCell) {
+	Ocean ocean;
+	ocean.setCellAtCoordinates(1, 1, Cell::SUNKEN_SHIP);
+	EXPECT_EQ(ocean.getCellAtCoordinates(1, 1), Cell::SUNKEN_SHIP);
+}
